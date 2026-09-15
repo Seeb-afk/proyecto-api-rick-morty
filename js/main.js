973c18f7f69794dba9266ffbaeb1ed0ed1c3e7be
Seeb-afk <sebastianpadron1@gmail.com>
2026-09-15 19:22:42 -0400

    Update: cambios de carpetas y agregando cartas de main

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 import { dataCharacters } from "../services/characters.js";
 
+async function showCharacters() {
+  const cards = document.getElementById("cards-container");
+  dataCharacters.results.forEach((character) => {
 
-const cards = document.getElementById("cards-container");
-dataCharacters.results.forEach((character) => {
+    const cardHTML = `
+      <article class="flex flex-col md:flex-row w-full bg-white rounded-md shadow-md overflow-hidden md:h-52">
 
-  const cardHTML = `
-    <div class="flex items-center justify-center bg-white p-2 gap-2 rounded-md shadow-sm">
-      
-    </div>`;
+        <img src="${character.image}" alt="${character.name}" class="w-full md:w-44 md:h-full md:object-cover shrink-0">
+
+        <div class="flex-1 flex flex-col p-4">
+          <h2 class="text-lg font-bold text-[#20232A] hover:text-[#3B82F6] active:text-[#2563EB] cursor-pointer">${character.name}</h2>
+
+          <div class="flex items-center gap-2 mt-1">
+            <span class="font-medium text-sm">${character.status}</span>
+            <span class="text-sm text-gray-500">&bull;</span>
+            <p class="text-sm text-gray-600">${character.species}</p>
+          </div>
+
+          <div class="mt-auto pt-4">
+            <p class="text-xs font-semibold text-gray-500 tracking-wide">ORIGEN:</p>
+            <p class="text-sm font-semibold">${character.origin.name}</p>
+          </div>
+        </div>
+
+      </article>`;
   
-  //cards.insertAdjacentHTML("beforeend", cardHTML);
-})
+    cards.insertAdjacentHTML("beforeend", cardHTML);
+  });
+};
+
+showCharacters();
